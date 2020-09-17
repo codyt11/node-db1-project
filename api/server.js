1daad6c389600f453c8ea1e73e86eecd8e655749
Codyt11 <Codyt11@hotmail.com>
2020-09-16 23:32:20 -0500

    MVP with some Stretch

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
+const accountsRouter = require("../accountsRouter")
+
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
 
+server.get("/", (req, res) => {
+    res.status(200).json({message: "the server is running"})
+})
+
+server.use("/api/accounts", accountsRouter)
 module.exports = server;
